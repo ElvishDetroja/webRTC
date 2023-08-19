eb9e476041f2cc09685cd53b316694b1ce695e92
ElvishDetroja <TheElvishDetroja@gmail.com>
2023-08-19 12:19:22 +0530

    first version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        {/* <Route element={<AdminRoute />}>
+          <Route path="/admin/products" element={<AdminProducts />} />
+          <Route path="/admin/product/add" element={<AdminProductAdd />} />
+          <Route path="/admin/product/:id" element={<AdminProductEdit />} />
+        </Route> */}
+      </Routes>
+    </>
   );
 }
 
