ebcfefa4a1a5448da49622eaf97b5841bb9d3f9a
fenil <fenilsavani1111@gmail.com>
2023-12-05 19:09:32 +0530

    video2 add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Video from "./pages/video";
+import Video2 from "./pages/video2";
+
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/video" element={<Video />} />
+        <Route path="/video2" element={<Video2 />} />
       </Routes>
     </>
   );
